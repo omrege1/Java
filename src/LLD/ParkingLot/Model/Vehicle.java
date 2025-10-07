cfbcbf69bd7a5855eced55b4f35831e570f4ecdd
Om Rege <aum.rege@gmail.com>
2025-10-07 10:23:41 +0530

    Parking Lot

--- src/LLD/ParkingLot/Model/Vehicle.java
+++ src/LLD/ParkingLot/Model/Vehicle.java
@@ -1,9 +1,5 @@
 package LLD.ParkingLot.Model;
 import LLD.ParkingLot.Model.VehicleConstants.VehicleType;
 
-public class Vehicle {
-        VehicleType type;
-        Vehicle(VehicleType type){
-            this.type = type;
-        }
+public record Vehicle(VehicleType type) {
 }
